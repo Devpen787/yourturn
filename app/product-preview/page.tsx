af0f730751e75b574138616fc961ba57b120453d
Devpen787 <Devpen787@gmail.com>
2026-09-11 03:14:14 +0200

    feat: route XC-03 provider lifecycle

--- app/product-preview/page.tsx
+++ app/product-preview/page.tsx
@@ -4,10 +4,15 @@ import { useSearchParams } from "next/navigation";
 import GoldenRecoveryClient from "./GoldenRecoveryClient";
 import Xc01Client from "./Xc01Client";
 import CompletionClient from "./CompletionClient";
+import ProviderLifecycleClient from "./ProviderLifecycleClient";
 
 export default function ProductPreviewPage() {
   const searchParams = useSearchParams();
   const view = searchParams.get("view");
+
+  if (view?.startsWith("xc3-")) {
+    return <ProviderLifecycleClient />;
+  }
 
   if (view?.startsWith("xc2-")) {
     return <CompletionClient />;
