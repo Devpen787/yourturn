b299c211da09f307d846debc64b66b0d9002880d
Devpen787 <Devpen787@gmail.com>
2026-09-11 03:11:16 +0200

    feat: route XC-02 fulfilment journey

--- app/product-preview/page.tsx
+++ app/product-preview/page.tsx
@@ -3,10 +3,15 @@
 import { useSearchParams } from "next/navigation";
 import GoldenRecoveryClient from "./GoldenRecoveryClient";
 import Xc01Client from "./Xc01Client";
+import CompletionClient from "./CompletionClient";
 
 export default function ProductPreviewPage() {
   const searchParams = useSearchParams();
   const view = searchParams.get("view");
+
+  if (view?.startsWith("xc2-")) {
+    return <CompletionClient />;
+  }
 
   if (view?.startsWith("xc-")) {
     return <Xc01Client />;
